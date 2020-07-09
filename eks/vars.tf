0462fe2b190c45af95c72071d9f0412662ea3f6b
Phani Teja Marupaka <pmarupaka@google.com>
2020-07-08 17:35:50 -0700

    Initial commit

---
+++ eks/vars.tf
@@ -0,0 +1,16 @@
+variable "cluster-name" {
+  default = "terraform-eks-demo"
+  type    = string
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
